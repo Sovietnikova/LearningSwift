52d806ef7247a5aeca130eaed58cc9ef77c51e93
millenium2018 <Natalia@sergs-mbp.lan>
2018-12-13 14:57:18 -0800

    Add two variants of Jew and Stones

--- JewelsAndStones.playground/Contents.swift
+++ JewelsAndStones.playground/Contents.swift
@@ -18,3 +18,28 @@ let J = "abFt"
 let S = "AwBbtolhj"
 numJewelsInStones(J, S)
 
+func numJewelsInStones2(_ J: String, _ S: String) -> Int {
+    var answer = 0
+    
+    for index in S {
+        if J.contains(index) {
+        answer += 1
+        }
+    }
+    return answer
+}
+
+numJewelsInStones2(J, S)
+
+func numJewelsInStones3(_ J: String, _ S: String) -> Int {
+    var answer = 0
+    let setOfJew = Set<Character>(J)
+    
+    S.forEach { if setOfJew.contains($0) {
+        answer += 1
+        }
+    }
+    return answer
+}
+
+numJewelsInStones3(J, S)
